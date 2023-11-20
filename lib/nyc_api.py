3da9a6088f8f58061c00f1784982ed73f24bed94
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-11-20 18:08:04 +0300

    Complete Requesting

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -9,6 +9,17 @@ class GetPrograms:
 
     response = requests.get(URL)
     return response.content
+  
+  def program_school(self):
+    schools = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+      schools.append(program['agency'])
+    return schools
 
-programs = GetPrograms().get_programs()
-print(programs)
+# programs = GetPrograms().get_programs()
+programs = GetPrograms()
+school_list = set(programs.program_school())
+
+for school in school_list:
+  print(school)
